b47bfc94b7fc76fe8b5eef53c5b4deb5b13e12ac
Daniel Buompane <danielbuompane@gmail.com>
2016-02-05 13:36:04 -0500

    created multiplication method.

--- currency_converter.rb
+++ currency_converter.rb
@@ -26,6 +26,12 @@ class Currency
     end
   end
 
+  def *(other)
+    if @currency_code == other.currency_code
+      @amount = @amount * other.amount
+    end
+  end
+
   def currency_code
     @currency_code
   end
@@ -35,11 +41,11 @@ class Currency
   end
 
 end
-dollar = Currency.new(:USD, 1.00)
+dollar = Currency.new(:USD, 2.00)
 dollar2 = Currency.new(:USD, 3.00)
 euro = Currency.new(:EUR, 1.00)
 # byebug
-puts dollar2 - dollar
+puts dollar2 * dollar
 
 #Need instance variables for currency_code, amount, maybe one more?
 #Need methods for addition and equals.
