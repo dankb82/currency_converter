3ee2394f88af54176e6333dc347b095134f30dfe
Daniel Buompane <danielbuompane@gmail.com>
2016-02-05 16:18:12 -0500

    created currency symbol catch

--- currency_converter.rb
+++ currency_converter.rb
@@ -2,6 +2,15 @@
 require 'byebug'
 class Currency
   def initialize(currency_code, amount)
+  currency_symbols = {"$" => :USD, "€" => :EUR}
+    if @currency_code == nil
+       @currency_code = @amount[0]
+       @amount = @amount[1..-1].to_f
+    else
+      @amount = amount.to_f
+      @currency_code = currency_code.to_sym
+    end
+
     @currency_code = currency_code
     @amount = amount
   end
@@ -16,7 +25,8 @@ class Currency
 
   def +(other)
     if @currency_code == other.currency_code
-      @amount = @amount + other.amount
+      dollar3 = @amount + other.amount
+      Currency.new(@currency_code, dollar3)
     else false
     end
   end
@@ -46,11 +56,17 @@ class Currency
 end
 dollar = Currency.new(:USD, 2.00)
 dollar2 = Currency.new(:USD, 3.00)
+dollar3 = dollar + dollar2
+
 euro = Currency.new(:EUR, 1.00)
 currency_symbols = {"$" => :USD, "€" => :EUR}
+dollar3 = dollar + dollar2
+puts dollar.amount
+puts dollar2
+puts dollar3
 
 # byebug
-puts dollar == dollar2
+
 
 #Need instance variables for currency_code, amount, maybe one more?
 #Need methods for addition and equals.
