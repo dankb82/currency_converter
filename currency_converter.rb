c546a7c49aed08db8755e9c02179bb30c5139697
Daniel Buompane <danielbuompane@gmail.com>
2016-02-05 12:14:49 -0500

    created currency NOT equal to  another Currency object with different amount or currency code

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,20 +1,34 @@
+
+require 'byebug'
 class Currency
   def initialize(currency_code, amount)
     @currency_code = currency_code
     @amount = amount
   end
 
-  def equal(other_amount)
-    @currency_code == other_currency && @amount == other_amount
+  def equal(other_currency, other_amount)
+    if @currency_code == other_currency && @amount == other_amount
+      true
+    else
+      false
+    end
   end
 
+  def currency_code
+    @currency_code
+  end
 
   def amount
     @amount
   end
 
 end
-
 dollar = Currency.new(:USD, 1.00)
+dollar2 = Currency.new(:USD, 1.00)
+euro = Currency.new(:EUR, 1.00)
+# byebug
+puts dollar.currency_code == euro.currency_code
+puts dollar.amount == euro.amount
 
-puts dollar.amount
+#Need instance variables for currency_code, amount, maybe one more?
+#Need methods for addition and equals.
