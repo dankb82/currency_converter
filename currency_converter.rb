1223d954614ecdc77f01cafe5b5f87e4803dc2c2
Daniel Buompane <danielbuompane@gmail.com>
2016-02-07 13:57:15 -0500

    got currency division working

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,17 +1,22 @@
 require './currency.rb'
 require 'byebug'
 class CurrencyConverter
-  def initialize(sitfyfrytg)
-    @currency_rates = sitfyfrytg
-
-
-
+  def initialize(currency_rates)
+    @rates = currency_rates
   end
 
 # byebug
   def convert(given_currency, desired_currency)
-    # byebug
-    new_amount = given_currency.amount * @currency_rates[desired_currency]
-    converted_currency = Currency.new(new_amount, desired_currency)
+    if @rates[desired_currency] > 1.0
+      new_amount = (given_currency.amount) * (@rates[desired_currency])
+      converted_currency = Currency.new(new_amount, desired_currency)
+    else
+      @rates[desired_currency] <= 1.0
+      new_amount = given_currency.amount / @rates[given_currency.currency_code]
+      converted_currency = Currency.new(new_amount, desired_currency)
+    end
   end
 end
+
+
+  # rates = {:USD => 1.00, :EUR => 0.89, :YEN => 116.8}
--- execute.rb
+++ execute.rb
@@ -1,3 +1,4 @@
+require "byebug"
 require './currency.rb'
 require './errors.rb'
 require './currency_converter.rb'
@@ -9,12 +10,15 @@ require './currency_converter.rb'
 
 
 
-dollar = Currency.new(2.00, :USD)
-moneyz = Currency.new("$3000")
-puts moneyz
-puts moneyz.amount
-rates = {:USD => 1.00, :EUR => 0.89}
-bank = CurrencyConverter.new(rates)
-new_money =  bank.convert(moneyz, :EUR)
+yen = Currency.new(1168.0, :YEN)
+# moneyz = Currency.new("$3000")
+# puts moneyz
+# puts moneyz.amount
+currency_rates = {:USD => 1.00, :EUR => 0.89, :YEN => 116.8}
+bank = CurrencyConverter.new(currency_rates)
+new_money = bank.convert(yen, :USD)
+# byebug
+puts new_money.amount
 puts new_money.currency_code
-puts new_money.amount == Currency.new(2670, :EUR).amount
+# puts new_money.currency_code
+# puts new_money.amount == Currency.new(2670, :EUR).amount
