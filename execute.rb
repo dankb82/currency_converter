df60cd96f831abb66565f9b1165817eac965aabd
Daniel Buompane <danielbuompane@gmail.com>
2016-02-06 22:11:29 -0500

    created conversion rates hash to be passed into currency converter class.

--- execute.rb
+++ execute.rb
@@ -1,2 +1,20 @@
-require './currency_converter.rb'
+require './currency.rb'
 require './errors.rb'
+require './currency_converter.rb'
+
+# dollar = Currency.new(2.00, :USD)
+# dollar2 = Currency.new(3.00, :USD)
+# dollar3 = dollar + dollar2
+# euro = Currency.new(1.00, :EUR)
+
+
+
+dollar = Currency.new(2.00, :USD)
+moneyz = Currency.new("$3000")
+puts moneyz
+puts moneyz.amount
+rates = {:USD => 1.00, :EUR => 0.89}
+bank = CurrencyConverter.new(rates)
+new_money =  bank.convert(moneyz, :EUR)
+puts new_money.currency_code
+puts new_money.amount == Currency.new(2670, :EUR).amount
